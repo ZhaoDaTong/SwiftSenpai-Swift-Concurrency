b5879020d96f60ffd1dfec400c5465207347381a
zhaobo <leo.zhao@gpower.co>
2023-09-25 15:49:36 +0800

    增加Actor及Task Group 测试代码

--- SwiftSenpai-Swift-Concurrency/Prevent Data Races/ActorViewController.swift
+++ SwiftSenpai-Swift-Concurrency/Prevent Data Races/ActorViewController.swift
@@ -7,17 +7,36 @@
 
 import UIKit
 
-class ActorViewController: UIViewController {
+// MARK: Counter Actor
+@MainActor
+struct Test {
+    
+}
 
-    // MARK: Counter Actor
-    actor Counter {
-        
-        private(set) var count = 0
-        
-        func addCount() {
-            count += 1
-        }
+@MainActor
+class TestClass {
+    
+}
+
+actor Counter {
+    let name: String
+    
+    private(set) var count = 0
+    
+    init(name: String) {
+        self.name = name
     }
+    
+    nonisolated func getName() -> String {
+        return name
+    }
+    
+    func addCount() {
+        count += 1
+    }
+}
+
+class ActorViewController: UIViewController {
     
     // MARK: Implementation
     @IBOutlet weak var statusLabel: UILabel!
@@ -27,11 +46,13 @@ class ActorViewController: UIViewController {
     }
     
 
+    @MainActor
     @IBAction func startButtonTapped(_ sender: Any) {
         
         let totalCount = 1000
-        let counter = Counter()
-
+        let counter = Counter(name: "eee")
+        print(counter.getName())
+        
         // Create a parent task
         Task {
             
@@ -49,6 +70,126 @@ class ActorViewController: UIViewController {
             statusLabel.text = "\(await counter.count)"
         }
    
+//        testTaskGroup()
+//        testAsyncLetBinding()
+    }
+    
+    func request() {
+        Task {
+            await testAsync()
+            
+            do {
+                let testStr = try await testAsyncAndThrows()
+                print(testStr as Any)
+            } catch {
+                
+            }
+        }
+    }
+    
+    func testAsync() async {
+        try? await Task.sleep(nanoseconds: 3 * 1_000_000_000)
+    }
+    
+    func testAsyncAndThrows() async throws -> String {
+        return "test"
     }
 
+    func testAsyncLetBinding() {
+        Task {
+            async let a = performTaskA()
+            async let b = performTaskB()
+            let sum = await(a + b)
+            print(sum)
+        }
+    }
+    
+    func testTaskGroup() {
+        print("Group start")
+        Task {
+            let result = await withTaskGroup(of: Int.self, returning: Int.self, body: {
+                taskGroup in
+                @Sendable func performTaskA() async -> Int {
+                    try? await Task.sleep(nanoseconds: 2 * 1_000_000_000)
+                    print("A complete")
+                    return 2
+                }
+
+                @Sendable func performTaskB() async -> Int {
+                    try? await Task.sleep(nanoseconds: 3 * 1_000_000_000)
+                    print("B complete")
+                    return 3
+                }
+
+                for index in 0...1 {
+                    taskGroup.addTask {
+                        if index == 0 {
+                            let value = await performTaskA()
+                            return value
+                        } else {
+                            let value = await performTaskB()
+                            return value
+                        }
+                    }
+                }
+
+                var sum = 0
+                for await result in taskGroup {
+                    sum += result
+                }
+
+                return sum
+            })
+
+            print("Group end - \(result)")
+        }
+        
+        Task {
+            await withTaskGroup(of: Int.self, body: {
+                taskGroup in
+                @Sendable func performTaskA() async -> Int {
+                    try? await Task.sleep(nanoseconds: 2 * 1_000_000_000)
+                    print("A complete")
+                    return 2
+                }
+                
+                @Sendable func performTaskB() async -> Int {
+                    try? await Task.sleep(nanoseconds: 3 * 1_000_000_000)
+                    print("B complete")
+                    return 3
+                }
+                
+                for index in 0...1 {
+                    taskGroup.addTask {
+                        if index == 0 {
+                            let value = await performTaskA()
+                            return value
+                        } else {
+                            let value = await performTaskB()
+                            return value
+                        }
+                    }
+                }
+                
+                var sum = 0
+                for await result in taskGroup {
+                    sum += result
+                }
+                
+                print("Group end - \(sum)")
+            })
+        }
+    }
+    
+    @Sendable func performTaskA() async -> Int {
+        try? await Task.sleep(nanoseconds: 2 * 1_000_000_000)
+        print("A complete")
+        return 2
+    }
+    
+    @Sendable func performTaskB() async -> Int {
+        try? await Task.sleep(nanoseconds: 3 * 1_000_000_000)
+        print("B complete")
+        return 3
+    }
 }
